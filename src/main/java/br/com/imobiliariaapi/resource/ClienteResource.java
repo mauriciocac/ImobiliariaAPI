42c3dc816ef05abf02c72b4b2640c917a3bd3326
Mauricio Carvalho <mauricio.cac@hotmail.com>
2020-09-15 19:12:12 -0300

    Commiting response of api

---
+++ src/main/java/br/com/imobiliariaapi/resource/ClienteResource.java
@@ -0,0 +1,32 @@
+package br.com.imobiliariaapi.resource;
+
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.imobiliariaapi.model.Customer;
+
+@RequestMapping("/Customers")
+@RestController
+public class ClienteResource {
+
+	private RestTemplate template;
+
+	@GetMapping
+	public ResponseEntity<List<Customer>> getApi() {
+		String api = "https://smarts-totem.s3-sa-east-1.amazonaws.com/code-challenge/customers.json";
+		template = new RestTemplate();
+
+		ResponseEntity<List<Customer>> customers = template.exchange(api, HttpMethod.GET, null, new ParameterizedTypeReference<List<Customer>>() {
+		 
+		});
+		 
+		return customers;
+	}
+}
